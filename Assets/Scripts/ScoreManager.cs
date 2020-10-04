b19d45cf87b5b52c1856786ee68ba9c3336e9c60
YannickMontes <montes.yannick@gmail.com>
2020-10-04 14:52:18 +0200

    Add basic score based on time

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -8,10 +8,59 @@ public class ScoreManager : Singleton<ScoreManager>
 {
 	public int Score { get { return m_score; } }
 
-	public void Reset()
+	private void ResetScore()
 	{
 		m_score = 0;
 	}
+
+	private void Start()
+	{
+		GameMaster.Instance.GameStateRelay.AddListener(OnGameStateChanged);
+	}
+
+	private void OnDestroy()
+	{
+		GameMaster.Instance?.GameStateRelay.RemoveListener(OnGameStateChanged);
+	}
+
+	private void OnGameStateChanged(GameMaster.GameState gameState)
+	{
+		switch (gameState)
+		{
+			case GameMaster.GameState.IN_GAME:
+				ResetScore();
+				StartCoroutine(ScoreOnTime());
+				break;
+
+			case GameMaster.GameState.END_SCREEN:
+				StopAllCoroutines();
+				break;
+		}
+	}
+
+	private void StartGame()
+	{
+		StartCoroutine(ScoreOnTime());
+	}
+
+	private void StopGame()
+	{
+		StopAllCoroutines();
+	}
+
+	private IEnumerator ScoreOnTime()
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds(m_timeUnit);
+			m_score += m_scorePerTimeUnit;
+		}
+	}
+
+	[SerializeField]
+	private int m_scorePerTimeUnit = 1;
+	[SerializeField]
+	private float m_timeUnit = 0.25f;
 
 	[NonSerialized]
 	private int m_score = 0;
---
+++ Assets/Scripts/UI/InGamePage.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class InGamePage : MonoBehaviour
+{
+	private void Update()
+	{
+		m_scoreText.text = $"Score {ScoreManager.Instance.Score}";
+	}
+
+	[SerializeField]
+	private TextMeshProUGUI m_scoreText = null;
+}
